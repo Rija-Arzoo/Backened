87b2352df8b33d8b80b148108ba7a6285b67519e
Rija-Arzoo <arzoorija555@gmail.com>
2026-05-16 10:25:10 +0500

    multer and cloudinary usage

---
+++ Backend/Backend Project/src/utils/Cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            //return "Could not find the path of file.";
+            return null;
+        }
+        //upload on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+            overwrite: true,
+            notification_url: "https://mysite.example.com/notify_endpoint"
+        })
+        //file has uploaded sucessfully
+        console.log("file has uploaded sucessfully", response.url)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove locally saved temporary file as the upload operation got failed.
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
